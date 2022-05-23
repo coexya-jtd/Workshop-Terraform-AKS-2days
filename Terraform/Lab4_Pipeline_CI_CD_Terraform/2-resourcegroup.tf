d6a6e04c65aa9f353fd0126e7ca45a1f9e0177da
jtd <jean-philippe.tisserand@coexya.eu>
2022-05-23 15:49:31 +0200

    Deploy VM

--- Terraform/Lab4_Pipeline_CI_CD_Terraform/2-resourcegroup.tf
+++ Terraform/Lab4_Pipeline_CI_CD_Terraform/2-resourcegroup.tf
@@ -18,3 +18,96 @@ resource "azurerm_resource_group" "Terra_tfbackend_rg" {
         Responsable = "AML"
     }
 }
+
+resource "azurerm_virtual_network" "terra_vnet" {
+  name                = var.vnetName
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.Terra_tfbackend_rg.location
+  resource_group_name = azurerm_resource_group.Terra_tfbackend_rg.name
+  tags = {
+        Environment = "JTD-K8S",
+        Client      = "Coexya DIA",
+        Responsable = "AML"
+    }
+}
+
+resource "azurerm_subnet" "terra_subnet" {
+  name                 = var.subnetName
+  resource_group_name  = azurerm_resource_group.Terra_tfbackend_rg.name
+  virtual_network_name = azurerm_virtual_network.terra_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "LabK8SPublicIp1"
+  resource_group_name = azurerm_resource_group.Terra_tfbackend_rg.name
+  location            = azurerm_resource_group.Terra_tfbackend_rg.location
+  allocation_method   = "Static"
+
+  tags = {
+    Environment = "JTD-K8S",
+    Client      = "Coexya DIA",
+    Responsable = "AML"
+  }
+}
+
+resource "azurerm_network_interface" "terra_nic" {
+  name                = var.nicName
+  location            = azurerm_resource_group.Terra_tfbackend_rg.location
+  resource_group_name = azurerm_resource_group.Terra_tfbackend_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.terra_subnet.id
+    private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+        Environment = "JTD-K8S",
+        Client      = "Coexya DIA",
+        Responsable = "AML"
+    }
+}
+
+resource "azurerm_linux_virtual_machine" "terra_vm" {
+  name                = var.vmName
+  resource_group_name = azurerm_resource_group.Terra_tfbackend_rg.name
+  location            = azurerm_resource_group.Terra_tfbackend_rg.location
+  size                = var.vmSize
+  admin_username      = var.vmUser
+  network_interface_ids = [
+    azurerm_network_interface.terra_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.vmUser
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical" # az vm image list --output table
+    offer     = "UbuntuServer" # az vm image list --offer UbuntuServer --all --output table
+    sku       = "18.04-LTS" # az vm image list-skus --location westus --publisher Canonical --offer UbuntuServer --output table
+    version   = "latest"
+  }
+
+  tags = {
+        Environment = "JTD-K8S",
+        Client      = "Coexya DIA",
+        Responsable = "AML"
+    }
+}
+
+
+
+
+
+
+
