d8527400cbc1d682d7e11b6a2750e640f9fc4c4e
jtd <jean-philippe.tisserand@coexya.eu>
2022-05-24 11:40:53 +0200

    Add tags

--- Terraform/Lab6_Azure_Kubernetes_Service/3-aks-cluster.tf
+++ Terraform/Lab6_Azure_Kubernetes_Service/3-aks-cluster.tf
@@ -33,6 +33,13 @@ resource "azurerm_kubernetes_cluster" "Terra_aks" {
     min_count           = var.defaultpool-mincount
     max_count           = var.defaultpool-maxcount
     vnet_subnet_id      = azurerm_subnet.Terra_aks_subnet.id
+
+     tags = {
+        Usage       = "WorkShop IaC"
+        Environment = "JTD-K8S",
+        Client      = "Coexya DIA",
+        Responsable = "AML"
+    }
   }
 
 #   linux_profile {
@@ -49,6 +56,13 @@ resource "azurerm_kubernetes_cluster" "Terra_aks" {
     docker_bridge_cidr = "172.17.0.1/16"
     service_cidr       = "10.0.0.0/16" # Must not overlap any address from the VNet
     load_balancer_sku  = "Standard"    # sku can be basic or standard. Here it an AKS cluster with AZ support so Standard SKU is mandatory
+
+     tags = {
+        Usage       = "WorkShop IaC"
+        Environment = "JTD-K8S",
+        Client      = "Coexya DIA",
+        Responsable = "AML"
+    }
   }
 
   # addon_profile {
